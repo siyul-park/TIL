d9286b23b4dfe77b492009dd5219bb4efbb6e430
Si-Yual Park <siyual.bak@gmail.com>
2019-05-05 15:39:11 +0900

    2019-05-04

--- Algorithm/src/백준 10844번 쉬운 계단 수.cpp
+++ Algorithm/src/백준 10844번 쉬운 계단 수.cpp
@@ -1,43 +1,44 @@
 #include "iostream"
 
-constexpr long max = 100;
+constexpr int MAX = 100;
 
-long howManyNumberHaveCache[max][10] = {0};
+long howManyNumberHaveStepNumber(int length, int number) {
+    static long cache[MAX][10] = {0};
 
-long howManyNumberHave(long n, long number) {
     if (number < 0 || number > 9)
         return 0;
 
-    if (howManyNumberHaveCache[n][number])
-        return howManyNumberHaveCache[n][number];
+    int lengthIndex = length - 1;
 
-    if (n == 0) {
+    if (cache[lengthIndex][number])
+        return cache[lengthIndex][number];
+
+    if (lengthIndex == 0) {
         if (number == 0) {
-            howManyNumberHaveCache[n][number] = 0L;
-            return howManyNumberHaveCache[n][number];
+            cache[lengthIndex][number] = 0L;
+            return cache[lengthIndex][number];
         }
 
-        howManyNumberHaveCache[n][number] = 1L;
-        return howManyNumberHaveCache[n][number];
+        cache[lengthIndex][number] = 1L;
+        return cache[lengthIndex][number];
     }
 
-    howManyNumberHaveCache[n][number] = (howManyNumberHave(n - 1, number - 1) + howManyNumberHave(n - 1, number + 1)) % 1000000000;
-    return howManyNumberHaveCache[n][number];
+    cache[lengthIndex][number] = (howManyNumberHaveStepNumber(length - 1, number - 1) + howManyNumberHaveStepNumber(length - 1, number + 1)) % 1000000000;
+    return cache[lengthIndex][number];
 }
 
-long getNumberOfStairNumber(long n) {
+long getNumberOfStepNumber(int length) {
     long sum = 0L;
     for (int i = 0; i < 10; ++i)
-        sum += howManyNumberHave(n, i);
+        sum += howManyNumberHaveStepNumber(length, i);
 
     return sum % 1000000000;
 }
 
-
 int main() {
-    long N;
+    int N;
     std::cin >> N;
 
-    std::cout << getNumberOfStairNumber(N - 1);
+    std::cout << getNumberOfStepNumber(N);
     return 0;
 }
