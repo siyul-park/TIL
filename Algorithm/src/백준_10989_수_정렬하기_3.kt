d4a5906f93cbd1d6a388b636546ccead52a9432b
박시율 <kdpark0723@gmail.com>
2020-02-23 15:45:12 +0900

    백준 10989 수 정렬하기

---
+++ Algorithm/src/백준_10989_수_정렬하기_3.kt
@@ -0,0 +1,20 @@
+import java.util.*
+import java.io.*
+
+fun BufferedReader.nextInt() = Integer.parseInt(this.readLine())
+
+fun main(args: Array<String>) {
+    val inputBuffer = BufferedReader(InputStreamReader(System.`in`))
+
+    val N = inputBuffer.nextInt()
+    val counter = MutableList(10_000 + 1) { 0 }
+
+    (0 until N).forEach { counter[inputBuffer.nextInt()] += 1 }
+
+    inputBuffer.close()
+
+    val outputBuffer = BufferedWriter(OutputStreamWriter(System.out))
+    counter.forEachIndexed { i, count -> (0 until count).forEach { outputBuffer.write("$i\n") } }
+
+    outputBuffer.close();
+}
